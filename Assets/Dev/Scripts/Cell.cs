71a78e2fa088ea165f2e0375325be603e0244436
Sachin18ss <sachin38541@gmail.com>
2026-02-19 12:25:33 +0530

    Visual Update
    - X/O can be drawn visually
    - X/O are turn based
    - X/O are currently text objects
    - X/O have different colors

--- Assets/Dev/Scripts/Cell.cs
+++ Assets/Dev/Scripts/Cell.cs
@@ -1,26 +1,33 @@
 using UnityEngine;
-
+using TMPro;
 public class Cell : MonoBehaviour
 {
     public int row;
     public int column;
 
+    [SerializeField] private TextMeshPro _cellText;
     [Header("Scripts Reference")]
     private GridManager gridManager;
 
     private void Start()
     {
-        gridManager = FindFirstObjectByType<GridManager>();   
+        gridManager = FindFirstObjectByType<GridManager>();
+        _cellText = GetComponentInChildren<TextMeshPro>(); ;
     }
     
+    public void SetValue(string value)
+    {
+        _cellText.color = value == "O" ? Color.blue : Color.red;
+        _cellText.text = value;
+    }
 
     void OnMouseDown()
     {
-        //Debug.Log("Cell clicked");
         if (gridManager.IsCellEmpty(row, column))
         {
-            gridManager.SetCell(row, column, 1);
-            Debug.Log($"Clicked {row},{column}");
+            gridManager.OnCellCliked(this);
+            //gridManager.SetCell(row, column, 1);
+            //Debug.Log($"Clicked {row},{column}");
         }
         
     }
--- Assets/Dev/Scripts/GridManager.cs
+++ Assets/Dev/Scripts/GridManager.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
-    private int rows = 3, cloumns = 3;
-    private int[,] grid;
+    private int _rows = 3, _cloumns = 3;
+    private int[,] _grid;
+    private bool _isXTurn = true;
 
     public GameObject cellPrefab;
     public float cellSize = 1f;
 
+
     private void Awake()
     {
-        grid = new int[rows, cloumns];
+        _grid = new int[_rows, _cloumns];
     }
 
     private void Start()
@@ -20,19 +22,19 @@ public class GridManager : MonoBehaviour
 
     public bool IsCellEmpty(int r, int c)
     {
-        return grid[r, c] == 0;
+        return _grid[r, c] == 0;
     }
 
     public void SetCell(int r, int c, int value)
     {
-        grid[r, c] = value;
+        _grid[r, c] = value;
     }
 
     public void DrawGrids()
     {
-        for(int r = 0; r<rows; r++)
+        for(int r = 0; r<_rows; r++)
         {
-            for (int c = 0; c < cloumns; c++)
+            for (int c = 0; c < _cloumns; c++)
             {
                 Vector2 worldPos = new Vector2(r * cellSize, c * cellSize);
 
@@ -44,4 +46,26 @@ public class GridManager : MonoBehaviour
             }
         }
     }
+
+    public void OnCellCliked(Cell cell)
+    {
+        int r = cell.row;
+        int c = cell.column;
+
+        if (_grid[r, c] != 0) return;
+
+        if(_isXTurn)
+        {
+            SetCell(r, c, 1);
+            cell.SetValue("X");
+        }
+        else
+        {
+            SetCell(r, c, 2);
+            cell.SetValue("O");
+        }
+
+        _isXTurn = !_isXTurn;
+    }
+
 }
